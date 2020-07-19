ff34b1e431f61c51bdfb1959ad936406b0e1af5e
Nguyen Van Nghia <nghia.nguyen@tp.com.vn>
2020-07-19 10:58:48 +0700

    Session21, Load MVC & trang fontend

--- php47_project/Backend/Views/new.php
+++
@@ -1,8 +0,0 @@
-<tr>
-	<td><?php echo $rowsSub->name; ?></td>
-	<td style="text-align:center;">
-		<a href="index.php?controller=categories&action=update&id=<?php echo $rowsSub->id; ?>">Edit</a>&nbsp;
-		<a href="index.php?controller=categories&action=delete&id=<?php echo $rowsSub->id; ?>"
-		   onclick="return window.confirm('Are you sure?');">Delete</a>
-	</td>
-</tr>
